1cee3d024b6a740b1d19268fccd77354f84ee81c
dudeisbrendan03 <jbrendan70@outlook.com>
2018-03-18 09:49:08 +0000

    Update ztui.py

--- ztui.py
+++ ztui.py
@@ -37,62 +37,67 @@ if clr == 'clear':
 #    else:
 #        print('OK.')
 
-while True:
-    c()
-    print("""
-    ███████╗███████╗██████╗  ██████╗ ████████╗██╗███████╗██████╗
-    ╚══███╔╝██╔════╝██╔══██╗██╔═══██╗╚══██╔══╝██║██╔════╝██╔══██╗
-      ███╔╝ █████╗  ██████╔╝██║   ██║   ██║   ██║█████╗  ██████╔╝
-     ███╔╝  ██╔══╝  ██╔══██╗██║   ██║   ██║   ██║██╔══╝  ██╔══██╗
-    ███████╗███████╗██║  ██║╚██████╔╝   ██║   ██║███████╗██║  ██║
-    ╚══════╝╚══════╝╚═╝  ╚═╝ ╚═════╝    ╚═╝   ╚═╝╚══════╝╚═╝  ╚═╝
+try:
+    while True:
+        c()
+        print("""
+        ███████╗███████╗██████╗  ██████╗ ████████╗██╗███████╗██████╗
+        ╚══███╔╝██╔════╝██╔══██╗██╔═══██╗╚══██╔══╝██║██╔════╝██╔══██╗
+          ███╔╝ █████╗  ██████╔╝██║   ██║   ██║   ██║█████╗  ██████╔╝
+         ███╔╝  ██╔══╝  ██╔══██╗██║   ██║   ██║   ██║██╔══╝  ██╔══██╗
+        ███████╗███████╗██║  ██║╚██████╔╝   ██║   ██║███████╗██║  ██║
+        ╚══════╝╚══════╝╚═╝  ╚═╝ ╚═════╝    ╚═╝   ╚═╝╚══════╝╚═╝  ╚═╝
 
-    GUI USING CLI CLIENT                         | NOT OFFICIAL
-    =================================================================
-    1) Join Network                     4) List Networks
-    2) Leave Network                    5) List Moons
-    3) List peers                       6) Bad decision please get me out of here
-    """)
-    selection = input("SELECT?> ")
-    if selection == '1':
-        sid = input("Enter the network ID: ")
-        print('\n')
-        zt('join '+sid)
-        input("\nPress any key to return to the menu")
-    elif selection == '2':
-        sid = input("Enter the network ID: ")
-        print('\n')
-        zt('leave '+sid)
-        input("\nPress any key to return to the menu")
-    elif selection == '3':
-        print('\n')
-        zt('listpeers')
-        input("\nPress any key to return to the menu")
-    elif selection == '4':
-        print('\n')
-        zt('listnetworks')
-        input("\nPress any key to return to the menu")
-    elif selection == '5':
-        print('\n')
-        zt('listmoons')
-        input("\nPress any key to return to the menu")
-    elif selection == '6':
-        print('\n')
-        print("How sad. It was nice knowing you.")
-        w(1)
-        print("Goodbye")
-        w(2)
-        exit()
-    elif selection == '1337':
-        print('\n')
-        print("I <3 C0mmun1sm")
-    elif selection == '7':
-        print('\n')
-        print("ERROR: WRONG OPINION PLEASE TRY AGAIN")
-    else:
-        print('\n')
-        print("Mistakes are BaDDdDDdDDD")
-        w(1)
-        print("You typed something in wrong")
-        w(2)
-        input("\nPress any key to return to the menu")
+        GUI USING CLI CLIENT                         | NOT OFFICIAL
+        =================================================================
+        1) Join Network                     4) List Networks
+        2) Leave Network                    5) List Moons
+        3) List peers                       6) Bad decision please get me out of here
+        """)
+        selection = input("SELECT?> ")
+        if selection == '1':
+            sid = input("Enter the network ID: ")
+            print('\n')
+            zt('join '+sid)
+            input("\nPress any key to return to the menu")
+        elif selection == '2':
+            sid = input("Enter the network ID: ")
+            print('\n')
+            zt('leave '+sid)
+            input("\nPress any key to return to the menu")
+        elif selection == '3':
+            print('\n')
+            zt('listpeers')
+            input("\nPress any key to return to the menu")
+        elif selection == '4':
+            print('\n')
+            zt('listnetworks')
+            input("\nPress any key to return to the menu")
+        elif selection == '5':
+            print('\n')
+            zt('listmoons')
+            input("\nPress any key to return to the menu")
+        elif selection == '6':
+            print('\n')
+            print("How sad. It was nice knowing you.")
+            w(1)
+            print("Goodbye")
+            w(2)
+            exit()
+        elif selection == '1337':
+            print('\n')
+            print("I <3 C0mmun1sm")
+        elif selection == '7':
+            print('\n')
+            print("ERROR: WRONG OPINION PLEASE TRY AGAIN")
+        else:
+            print('\n')
+            print("Mistakes are BaDDdDDdDDD")
+            w(1)
+            print("You typed something in wrong")
+            w(2)
+            input("\nPress any key to return to the menu")
+except KeyboardInterrupt:
+    print("Goodbye :-(")
+    w(3)
+    exit()
